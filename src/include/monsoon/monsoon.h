b85a00609fb3f13ee76b29acaa2a522c43f2b858
Peter Bjorklund <piot@hotmail.com>
2021-01-01 17:11:49 +0100

    style: format

--- src/include/monsoon/monsoon.h
+++ src/include/monsoon/monsoon.h
@@ -1,12 +1,11 @@
 #include <opus/opusfile.h>
 #include <stdint.h>
 
-typedef struct Monsoon
-{
-    OggOpusFile *opusFile;
+typedef struct Monsoon {
+    OggOpusFile* opusFile;
 } Monsoon;
 
-int monsoonInit(Monsoon *self, uint32_t frequency, const uint8_t *data, int octetCount);
-int monsoonDecode(Monsoon *self, int16_t *sampleTarget, int sampleCount);
-int monsoonMinimumSampleBufferSize(const Monsoon *self);
+int monsoonInit(Monsoon* self, uint32_t frequency, const uint8_t* data, int octetCount);
+int monsoonDecode(Monsoon* self, int16_t* sampleTarget, int sampleCount);
+int monsoonMinimumSampleBufferSize(const Monsoon* self);
 int monsoonRewind(struct Monsoon* self);
